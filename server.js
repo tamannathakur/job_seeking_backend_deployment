4b8bfcbeea776c6b16a8b73448c3407294bcae5e
tamannathakur <tamannathakur2710@gmail.com>
2024-06-22 02:36:04 +0530

    commit

--- server.js
+++ server.js
@@ -12,3 +12,12 @@ cloudinary.v2.config({
 app.listen(process.env.PORT, () => {
   console.log(`Server running at port ${process.env.PORT}`);
 });
+const cors = require('cors');
+
+const corsOptions = {
+  origin: 'https://job-seeking-application-t.netlify.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Add methods your frontend uses
+  allowedHeaders: ['Content-Type', 'Authorization'], // Add headers your frontend sends
+};
+
+app.use(cors(corsOptions));
